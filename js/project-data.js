af0f54db26c7e0fb8b550f91a950d40444d47d57
Brian4755 <brian.mccune1@yahoo.com>
2022-10-10 16:27:47 -0500

    card containers

--- js/project-data.js
+++ js/project-data.js
@@ -1,5 +1,7 @@
+import { projectData } from "./works.js"
 const menuIcon = document.querySelector(".menuIcon")
 const menuList = document.querySelector(".navbarList")
+const cardContainer = document.getElementById('card-container')
 
 menuIcon.addEventListener('click', function(e) {
   menuIcon.classList.toggle("change")
@@ -36,3 +38,23 @@ setTimeout(function(){
   const element = document.querySelector(".preload");
   element.classList.remove("preload");
 }, 1000);
+
+const projectMaterial = projectData.map(project => {
+  const html = `  
+    <div class="card" style="width: 20rem;">
+      <img class="card-img-top" src="${project.image}" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${project.title}</h5>
+        <p class="card-text">${project.description}</p>
+        <div>
+          <a href="${project.gitHub}" target="_blank" class="btn btn-primary">GitHub</a>
+          <a href="${project.deployment}" target="_blank" class="btn btn-secondary">Deployment</a>
+        </div>
+      </div>
+    </div>
+  `
+  return html
+}).join('')
+
+
+cardContainer.innerHTML = projectMaterial
---
+++ js/works.js
@@ -0,0 +1,20 @@
+let projectData = [
+  {
+    title: 'Tic Tac Toe',
+    deployment: 'https://tictactoe-brian4755.netlify.app/',
+    image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/309620621_518068556331643_7820254715798910924_n.png?stp=dst-png_p403x403&_nc_cat=102&ccb=1-7&_nc_sid=aee45a&_nc_ohc=hLqZD8Fy-NAAX_79Yuj&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVIVI03l-vPEsTikaacUe5UNOxSiH7Ze6BejaKIxJdEK7w&oe=636A4761',
+    gitHub: 'https://github.com/Brian4755/ttt-weekend',
+    description: 'Tic Tac Toe game using JS, HTML, and CSS'
+  },
+  {
+    title: 'TFT CheatSheet',
+    deployment: 'https://tftcheatsheet.herokuapp.com/',
+    image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/290905112_996035144413563_3849728889679033423_n.png?stp=dst-png_p403x403&_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_ohc=cstpPu3mny8AX-cJmnj&_nc_oc=AQmIRcQHJgPKW9XTDYneaEUUXsS4dxE_QbssNlK5fbd9keS-WeNAGsMqRPrZP5vklHpKDSIBkFz2bzlyIq6Kyc2w&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVIO8PZonvQUYQ0m7wKkFNNNLqD6vLhQTUN6BU1V3MxtzQ&oe=63689762',
+    gitHub: 'https://github.com/Brian4755/TFTCheatSheet',
+    description: 'Forum/guide for a game called TeamFight Tactics. Used technologies such as EJS mongoDB/mongoose and JavaScript'
+  }
+]
+
+export {
+  projectData
+}
