494fc67b83d3aec103ab5b1b6b3eb8ec1f88b56a
Brian4755 <brian.mccune1@yahoo.com>
2022-10-18 20:45:32 -0500

    animation change and navbar placement

--- js/project-data.js
+++ js/project-data.js
@@ -22,6 +22,9 @@ const observer = new IntersectionObserver(function(entries, observer) {
   entries.forEach(entry => {
     if(!entry.isIntersecting) {
       return;
+    } else {
+      entry.target.classList.add('appear');
+      observer.unobserve(entry.target);
     }
     console.log(entry.target)
   })
