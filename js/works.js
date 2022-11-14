4651f8c71665e524da2e9840da838629adb52b77
Brian4755 <brian.mccune1@yahoo.com>
2022-11-14 07:41:39 -0600

    assets folder images

--- js/works.js
+++ js/works.js
@@ -2,14 +2,14 @@ let projectData = [
   {
     title: 'Tic Tac Toe',
     deployment: 'https://tictactoe-brian4755.netlify.app/',
-    image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/309620621_518068556331643_7820254715798910924_n.png?stp=dst-png_p403x403&_nc_cat=102&ccb=1-7&_nc_sid=aee45a&_nc_ohc=kk4a1KtB9QkAX_jgbY0&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSJeTK6PGYR6VxgcpYAhzpXtviYJqaiXJY9vqCVA-oYjw&oe=63994CE1',
+    image: '../assets/TicTacToe.png',
     gitHub: 'https://github.com/Brian4755/ttt-weekend',
     description: 'Tic Tac Toe game using JS, HTML, and CSS'
   },
   {
     title: 'TFT CheatSheet',
     deployment: 'https://tftcheatsheet.herokuapp.com/',
-    image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/290905112_996035144413563_3849728889679033423_n.png?stp=dst-png_p403x403&_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_ohc=cstpPu3mny8AX-cJmnj&_nc_oc=AQmIRcQHJgPKW9XTDYneaEUUXsS4dxE_QbssNlK5fbd9keS-WeNAGsMqRPrZP5vklHpKDSIBkFz2bzlyIq6Kyc2w&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVIO8PZonvQUYQ0m7wKkFNNNLqD6vLhQTUN6BU1V3MxtzQ&oe=63689762',
+    image: '../assets/TFT.png',
     gitHub: 'https://github.com/Brian4755/TFTCheatSheet',
     description: 'Forum/guide for a game called TeamFight Tactics. Used technologies such as EJS mongoDB/mongoose and JavaScript'
   }
