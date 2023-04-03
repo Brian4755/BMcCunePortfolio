6b30e4d32ba4d58e1a0b720601874a9ddb2f378d
Brian4755 <brian.mccune1@yahoo.com>
2023-04-03 07:22:06 -0500

    business site data

--- js/works.js
+++ js/works.js
@@ -1,10 +1,10 @@
 let projectData = [
   {
-    title: 'Tic Tac Toe',
-    deployment: 'https://tictactoe-brian4755.netlify.app/',
-    image: '../assets/TicTacToe.png',
-    gitHub: 'https://github.com/Brian4755/ttt-weekend',
-    description: 'Tic Tac Toe game using JS, HTML, and CSS'
+    title: 'React Business Site',
+    deployment: 'http://jacks-barbershop.herokuapp.com/',
+    image: '../assets/BarbershopWebsite.png',
+    gitHub: 'https://github.com/Brian4755/Jack-s-Barbershop',
+    description: 'Barbershop business website made with React and hooks such as useState and useEffect'
   },
   {
     title: 'TFT CheatSheet',
